02d8cf103dcc8e437af8f1275ed834780bed8897
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-03-04 12:03:25 +0100

    call route in starter file

--- app.js
+++ app.js
@@ -2,25 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3060
 const dotenv = require('dotenv')
+const request = require('./routes/request')
 dotenv.config()
 
-const { Configuration, OpenAIApi } = require("openai");
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-async function chat() {
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: "Hello world" }],
-    });
-    console.log(completion.data.choices[0].message);
-    // return completion.data.choices[0].message.content
-}
-
-chat()
+app.use('/api', request)
 
 app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`)
