8435a0fb695fceb92f20410891d425a6ba146f7c
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-03-04 12:02:48 +0100

    making changes

--- controllers/request.js
+++ controllers/request.js
@@ -1,12 +1,17 @@
 const openai = require('../config/config')
+const { ResponseHandler } = require('../utils');
 
 const request = async (req, res) => {
+
+    const { message } = req.body;
+    console.log(message)
+
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: "Hello world" }],
+        messages: [{ role: "user", content: `${message}` }]
     });
     // console.log(completion.data.choices[0].message);
-    res.json(completion.data.choices[0].message)
+    ResponseHandler(res, data = completion.data.choices[0].message, error = null, status = 200)
 }
 
 module.exports = request
