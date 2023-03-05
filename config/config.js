a0dbbf517caac8e74663a4e8ef50dcb607280ba5
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-03-05 23:52:24 +0100

    reviewed and changed configuration

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 dotenv.config()
+
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
