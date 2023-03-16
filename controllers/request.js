c658cd7c286f6da1544f1e676d7054959024cac8
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-03-16 22:58:23 +0100

    added catch for errors

--- controllers/request.js
+++ controllers/request.js
@@ -3,18 +3,19 @@ const { ResponseHandler } = require('../utils');
 
 
 const request = async (req, res) => {
-    const { message } = req.body
-    console.log(message)
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: `${message}` }],
-    });
-    // res.json({
-    //     // data: response.data
-    //     message: response.data.choices[0].text
-    // })
 
-    ResponseHandler(res, data = completion.data.choices[0].message, error = null, status = 200)
+    try {
+        const { message } = req.body
+        console.log(message)
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: `${message}` }],
+        });
+        ResponseHandler(res, data = completion.data.choices[0].message, error = null, status = 200)
+    } catch (err) {
+        ResponseHandler(res, data = 'There was an issue with the server...', error = err, status = 400)
+    }
+
 }
 
 module.exports = request
